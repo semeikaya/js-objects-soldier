b1255d5a2de74f8acfd89ddb06e0e8c2411401b5
Bers <semeikayaskaevih@gmail.com>
2022-08-31 12:02:33 +0300

    прописал характеристики персонажу. добавил действия, которые будет выполнять персонаж

---
+++ soldier.js
@@ -0,0 +1,18 @@
+const soldier = {
+    name: 'Gigachad',
+    health: 10,
+    weapon:{
+        model: 'Лук',
+        numberofrounds: 30,
+    } ,
+    ammo: 3,
+    fire: function () {
+        
+    },
+    reload: function () {
+        
+    },
+    hurt: function () {
+        
+    } ,
+}
