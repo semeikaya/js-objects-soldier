f6ab75c6ae72332dc8c398b4bbe30c061ebdaafd
Bers <semeikayaskaevih@gmail.com>
2022-08-31 12:04:05 +0300

    прописал методы

--- soldier.js
+++ soldier.js
@@ -7,12 +7,30 @@ const soldier = {
     } ,
     ammo: 3,
     fire: function () {
+        if (this.weapon.numberofrounds === 0) {
+            console.log("обойма пуста. Перезарядитесь");
+        }else{this.weapon.numberofrounds -= 1
+            console.log('бах-бах');}
         
     },
     reload: function () {
-        
+        if (this.ammo === 0 && this.weapon.numberofrounds < 30) {
+            console.log("не осталось припасов");
+        } else if (this.weapon.numberofrounds === 30) {
+            console.log("полная обойма");
+        }
+        else {
+            this.weapon.numberofrounds = 30
+            this.ammo -= 1
+            console.log('перезарядка');
+        }
     },
     hurt: function () {
-        
+        if (this.health === 0) {
+            console.log('mission failed');
+        }else{
+            this.health -= 1
+        }
+    
     } ,
 }
